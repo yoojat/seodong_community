7a207945ca346571ead15076ec49de25ae243855
yoojat <yooja.ty@gmail.com>
2024-12-01 12:16:05 +0900

    오류 의심 요소 삭제

--- app/(tabs)/layout.tsx
+++ app/(tabs)/layout.tsx
@@ -1,9 +1,3 @@
-// import TabBar from "@/components/tab-bar";
 export default function TabLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <div>
-      {children}
-      {/* <TabBar /> */}
-    </div>
-  );
+  return <div>{children}</div>;
 }
